3c603f5acf82dbb48242493c09c2a3ca5da1c77b
Shannon I'Ons <shannnon@dataprophet.com>
2021-12-02 15:35:12 +0200

    fix: Case fix.

--- day-1.py
+++ day-1.py
@@ -28,15 +28,17 @@ get_part_one_result = lambda file_path: reduce(
 
 # PART 2
 
-# left to right
+
 def compose(*functions):
     def __compose(initial_value):
-        return reduce(lambda result, value: value(result), functions, initial_value)
+        return reduce(
+            lambda result, value: value(result), functions[::-1], initial_value
+        )
 
     return __compose
 
 
-def toInt(iterable):
+def to_int(iterable):
     for x in iterable:
         yield int(x)
 
@@ -45,18 +47,15 @@ def take(take_num):
     def __take(iterable):
         take_list = []
         for x in iterable:
-            # add
             take_list = [x] + take_list
-            # remove
             take_list = take_list[0:take_num]
-            # yield if full
             if len(take_list) == take_num:
                 yield take_list
 
     return __take
 
 
-def isSumIncrease(slide_num, values):
+def is_sum_increase(slide_num, values):
     previous_sum = sum(values[0 : slide_num - 1])
     current_sum = sum(values[1:slide_num])
     return is_increase(previous_sum, current_sum)
@@ -65,12 +64,12 @@ def isSumIncrease(slide_num, values):
 def process_part_two(slide_num, file_path):
     return reduce(
         lambda result, values: result + 1
-        if isSumIncrease(slide_num, values)
+        if is_sum_increase(slide_num, values)
         else result,
-        compose(get_data, toInt, take(4))(file_path),
+        compose(take(4), to_int, get_data)(file_path),
         0,
     )
 
 
-print(process_part_two(4, "day-1-test-data.txt"))
+# print(process_part_two(4, "day-1-test-data.txt"))
 print(process_part_two(4, "day-1-data.txt"))
